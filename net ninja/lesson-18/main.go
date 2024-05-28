5250a175d1c0c269a400ac1063b8b29d58bdb0a4
iamitabh <amitabh.mishra.2002@gmail.com>
2024-05-28 23:00:57 +0530

    add net ninja lesson 18

---
+++ net ninja/lesson-18/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+
+	fmt.Println(prompt)
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
+
+func createBill() bill {
+
+	reader := bufio.NewReader(os.Stdin)
+
+	// fmt.Println("Create a new bill name: ")
+	// // This function captures the input from terminal till it's encounter \n or enter.
+	// name, _ := reader.ReadString('\n')
+	// name = strings.TrimSpace(name)
+
+	name, _ := getInput("Create a new bill name: ", reader)
+
+	b := newBill(name)
+	fmt.Println("Created the bill - ", b.name)
+
+	return b
+
+}
+
+func promptOptions(b bill) {
+
+	reader := bufio.NewReader(os.Stdin)
+
+	opt, _ := getInput("Choose Option (a - add item, s - save bill, t - add tip): ", reader)
+	fmt.Println(opt)
+
+}
+
+func main() {
+
+	mybill := createBill()
+	promptOptions(mybill)
+	fmt.Println(mybill)
+
+}
